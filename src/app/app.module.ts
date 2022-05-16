e7da297a427d152b98254324ab6b09d20860a02d
Julie <jatkin80@ford.com>
2022-05-16 15:15:08 -0500

    header has components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { WineListingComponent } from './wine-listing/wine-listing.component';
+import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WineListingComponent,
+    LogoComponent,
+    NavComponent,
+    UserAvatarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+@Input() homeUrl=""
+@Input() whitesUrl=""
+@Input() redsUrl=""
+@Input() blendsUrl=""
+}
